9fe9527a25c4f3866e16ac537144cf2a4c7df49e
Kaspar L. Palgi <git@e-stonia.co.uk>
2024-07-01 16:37:30 +0300

    Document repository.ts

--- src/infrastructure/repository.ts
+++ src/infrastructure/repository.ts
@@ -5,12 +5,18 @@ import { checkMissingProperties } from "../application";
 
 
 export class Repository {
-    async getJykood(jykood: number, landPercent?: number|undefined ): Promise<Yearly> {
+    /**
+     * Retrieves the yearly data for a given jykood.
+     * @param jykood - The jykood value.
+     * @param maaProtsent - Optional land percentage value.
+     * @returns A Promise that resolves to a Yearly object.
+     */
+    async getJykood(jykood: number, maaProtsent?: number|undefined ): Promise<Yearly> {
         const pg = `
             SELECT *
                 FROM "elujoulisuseindeks"."aastased"
                 WHERE "jykood" = ${jykood} 
-                ${landPercent ? `AND "maa_protsent" >= ${landPercent}` : ''}
+                ${maaProtsent ? `AND "maa_protsent" >= ${maaProtsent}` : ''}
                 ORDER BY "aasta" DESC
             LIMIT 1;
         `
@@ -18,7 +24,7 @@ export class Repository {
             SELECT *
                 FROM elujoulisuseindeks.aastased
                 WHERE jykood = ${jykood}
-                ${landPercent ? `AND maa_protsent >= ${landPercent}` : ''}
+                ${maaProtsent ? `AND maa_protsent >= ${maaProtsent}` : ''}
                 ORDER BY aasta DESC
             FETCH FIRST 1 ROWS ONLY;
         `
@@ -27,6 +33,12 @@ export class Repository {
         return result;
     }
 
+    /**
+     * Retrieves the YearlyCluster object for the specified klaster.
+     * 
+     * @param klaster - The klaster for which to retrieve the YearlyCluster object.
+     * @returns A Promise that resolves to the YearlyCluster object.
+     */
     async getSds(klaster: string): Promise<YearlyCluster> {
         const pg = `
             SELECT *
@@ -46,6 +58,11 @@ export class Repository {
         return result;
     }
 
+    /**
+     * Retrieves the YearlyCluster object for the specified klaster.
+     * @param klaster - The klaster identifier.
+     * @returns A Promise that resolves to the YearlyCluster object.
+     */
     async getMea(klaster: string): Promise<YearlyCluster> {
         const pg = `
             SELECT *
@@ -65,6 +82,11 @@ export class Repository {
         return result;
     }
 
+    /**
+     * Retrieves the monthly cluster data for a given cluster.
+     * @param klaster - The cluster identifier.
+     * @returns A promise that resolves to the monthly cluster data.
+     */
     async getMonthly(klaster: string): Promise<MonthlyCluster> {
         const pg = `
             SELECT *
